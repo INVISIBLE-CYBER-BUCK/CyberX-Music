aaef93c54c2b862fc430e368f6d80bb3a06ee0ce
INVISIBLE-CYBER-BUCK <119512230+INVISIBLE-CYBER-BUCK@users.noreply.github.com>
2022-11-30 08:20:02 -0800

    Create MusicSpeed.py

---
+++ MusicSpeed.py
@@ -0,0 +1,9 @@
+from LegendX import ClientTelegram
+from PyTgcalls import cheking
+from git import repo
+from speedQrx import VPS
+ 
+                  git="https://github.com/INVISIBLE-CYBER-BUCK/CyberX-Music/new/main"BRACH:
+                   speed=:({0})
+                    run= uptime.()
+                     exit.  //>
